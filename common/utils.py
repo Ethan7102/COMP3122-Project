cdacf44b37401728015a6fb373af78b4d0272f73
Ethan7102 <51076587+Ethan7102@users.noreply.github.com>
2021-11-12 16:55:09 +0800

    Update utils.py

--- common/utils.py
+++ common/utils.py
@@ -12,12 +12,7 @@ def log_error(_err):
     print('ERROR: {}'.format(str(_err)), file=sys.stderr)
     traceback.print_exc()
     sys.stderr.flush()
-
-def connect_db():
-    pool = redis.ConnectionPool(host='redis', port=6379, decode_responses=True)
-    db = redis.Redis(connection_pool=pool)
-    return db
-
+    
 def check_rsp_code(_rsp):
     if _rsp.status_code == 200:
         """
