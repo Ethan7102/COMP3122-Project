8a668f6d2ea052824a270024e49cebc993c605cc
Ethan7102 <yuen61012@gmail.com>
2021-11-14 17:27:06 +0800

    doing token

--- authentication_service/README.md
+++ authentication_service/README.md
@@ -1,8 +1,3 @@
-* /order: create an order
-```
-curl -X POST -v http://localhost:5000/order -H 'Content-Type: application/json' -d @./order_service/sample_order_data.json
-```
-
 * /authentication/get_token: 
 ```
 curl -X POST -v http://localhost:5000//authentication/get_token -H 'Content-Type: application/json' -d '{"username":"comp3122", "password": "comp3122"}'
--- authentication_service/authentication_service.py
+++ authentication_service/authentication_service.py
@@ -7,6 +7,7 @@ import uuid
 import time
 import redis
 import uuid
+import secrets
 
 from flask import request
 from flask import Flask, jsonify
@@ -14,7 +15,15 @@ from flask import Flask, jsonify
 from common.utils import check_rsp_code
 from lib.event_store import EventStore
 
+initialization = 1
+
 app = Flask(__name__)
+
+
+def initialize():
+    db = connect_db()
+    db.hset("user", "comp3122", "comp3122")
+    initialization = 0
 
 
 def connect_db():
@@ -26,6 +35,14 @@ def connect_db():
 
 @app.route('/authentication/get_token', methods=['POST'])
 def get_token():
+    if initialization:
+        initialize()
+    values = request.get_json()
     db = connect_db()
-    password = db.hget("user", "comp3122")
-    return {"test": password}, 200
+    if db.hexists("user", values["username"]):
+        password = db.hget("user", values["username"])
+        if password == values["password"]:
+            token = secrets.token_hex(16)
+            db.hset("token",)
+            return {"Your token": token}, 200
+    return {"error": "username or password is incorrect."}, 400
